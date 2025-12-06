2fc30f612e853578eb1b4f852f654ce80a2e163f
Andrew Baron <abaron@sbts.edu>
2025-12-06 13:36:11 -0500

    Adds solution to part 2
    Updates `README.md` with instructions for part 2
    
    First attempt: `6759`
    
    > That's not the right answer; your answer is too high. If you're stuck,
    make sure you're using the full input data; there are also some general
    tips on the about page, or you can ask for hints on the subreddit.
    Please wait one minute before trying again.
    
    Your puzzle answer was `6738`.
    
    Both parts of this puzzle are complete! They provide two gold stars: \*\*
    
    Refer to previous commit 79b67ea
    
    To the King of the ages, immortal, invisible, the only God, be honor and glory forever and ever. Amen.

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,48 @@
 use regex::Regex;
 
 pub fn calculate_dial(input: Vec<i32>, start: i32, size: i32) -> i32 {
-    let mut zeros = 0;
+    // Because the dial includes position "0" the length to be used by the algorithm is size + 1
+    let length = 1 + size;
 
-    println!("x	sum	result");
-    input.iter().fold(start, |acc, &x| {
-        let mut result = (1 + size + (acc + x)) % (1 + size);
+    let (_end_dial_position, zeros) = input.iter().fold((start, 0), |(position, zeros), &delta| {
+        let mut result = (length + (position + delta)) % length;
 
         // This should be resolved by the above equation itself
+        // Because the safe dial only has numbers 0 - 99 (or whatever size is),
+        // it does not make sense for there to be negative positions
         if result < 0 {
-            result += size + 1;
+            result += length;
         };
 
-        println!("{:?}	{:?}	{:?}", x, (acc + x), result);
-
-        // Count the number of zeros encountered
-        if 0 == result {
-            zeros += 1;
-        }
-
-        result
+        (result, zeros + count_zeros(length, position, delta))
     });
+
+    zeros
+}
+
+pub fn count_zeros(length: i32, position: i32, delta: i32) -> i32 {
+    let distance_to_zero = if 0 > delta {
+        position
+    } else {
+        length - position
+    };
+
+    // Calculate the offset necessary to move the dial position to "0"
+    let offset = distance_to_zero - delta.abs();
+
+    // Calculate the number of turns past zero
+    // This is the clever bit—once the dial is offset to "0" it does not matter which direction is
+    // traversed to calculate the number of revolutions the dial makes. Thus, the absolute value
+    // may be used to calculate the number of turns past zero for a given change in the dial.
+    let turns = offset.abs() / length;
+
+    let zeros = if 0 != distance_to_zero && delta.abs() >= distance_to_zero {
+        1 + turns
+    } else {
+        turns
+    };
+
+    println!("- position: {:?},	|delta|: {:?}, 	distance_to_zero: {:?},	offset: {:?},	turns: {:?},	{:?}", position, delta.abs(), distance_to_zero, offset, turns, zeros);
 
     zeros
 }
@@ -46,46 +68,31 @@ mod tests {
 
     #[test]
     fn test_example_input() {
-        let total = calculate_dial(vec![
-            -68,
-            -30,
-            48,
-            -5,
-            60,
-            -55,
-            -1,
-            -99,
-            14,
-            -82,
-        ], 50, 99);
+        let total = calculate_dial(
+            vec![-68, -30, 48, -5, 60, -55, -1, -99, 14, -82],
+            50,
+            99
+        );
 
-        assert_eq!(total, 3);
+        assert_eq!(total, 6);
     }
 
     #[test]
     fn test_example_modified_input() {
-        let total = calculate_dial(vec![
-            -68,
-            -30,
-            48,
-            -5,
-            60,
-            -55,
-            -1,
-            -99,
-            14,
-            -114,
-            -115,
-            -82,
-        ], 50, 99);
+        let total = calculate_dial(
+            vec![-68, -30, 48, -5, 60, -55, -1, -99, 14, -114, -115, -82, 99, -999],
+            50,
+            99
+        );
 
-        assert_eq!(total, 4);
+        assert_eq!(total, 19);
     }
 
     #[test]
     fn test_puzzle_input() {
         let total = calculate_dial(parse_numbers(fs::read_to_string("input.txt").expect("Failed to access file.")), 50, 99);
 
-        assert_eq!(total, 1150);
+        // assert_eq!(total, 6759);
+        assert_eq!(total, 6738);
     }
 }
